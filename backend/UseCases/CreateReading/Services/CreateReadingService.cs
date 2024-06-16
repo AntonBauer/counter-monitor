2b6a9f5d6a4cf3e2a366812679eb18b4331479fd
Anton Bauer <anton.bauer2@generic.de>
2024-06-16 13:17:43 +0200

    update general domain package

--- backend/UseCases/CreateReading/Services/CreateReadingService.cs
+++ backend/UseCases/CreateReading/Services/CreateReadingService.cs
@@ -1,4 +1,3 @@
-using CounterMotinor.Domain;
 using CounterMotinor.Domain.Entities.Counters;
 using CounterMotinor.Infrastructure.DataAccess;
 using CSharpFunctionalExtensions;
@@ -9,8 +8,8 @@ internal sealed class CreateReadingService(CounterMonitorContext context)
 {
     public async Task<Result<Counter, string>> Create(Guid counterId, DateTimeOffset date, double value, CancellationToken cancellationToken) =>
       await (await context.Counters.FindAsync(CounterId.CreateFrom(counterId),
-                                               cancellationToken)
-            ).ToResult($"Counter {counterId} not found")
-             .Tap(counter => counter.AddReading(date, value))
-             .Tap(async () => { await context.SaveChangesAsync(cancellationToken);});
+                                              cancellationToken))
+        .ToResult($"Counter {counterId} not found")
+        .Tap(counter => counter.AddReading(date, value))
+        .Tap(async () => { await context.SaveChangesAsync(cancellationToken);});
 }
